0c0483153e8c81c99fdd26f5aef05de4011e1772
Quibylix <barraza.fredimanuel@gmail.com>
2025-04-22 08:18:37 -0600

    feat: implement login api endpoint

--- src/app/api/v1/auth/login/route.ts
+++ src/app/api/v1/auth/login/route.ts
@@ -1,6 +1,8 @@
 import { ERROR_CODES } from "@/features/auth/login/error_codes.constant";
+import { login } from "@/features/auth/login/login.action";
 import { getTranslations } from "next-intl/server";
 import { NextRequest } from "next/server";
+import { z } from "zod";
 
 export type APIResponse = {
   success: boolean;
@@ -8,16 +10,45 @@ export type APIResponse = {
   message: string;
 };
 
+const bodySchema = z.object({
+  username: z.string().trim().nonempty(),
+  password: z.string().nonempty(),
+});
+
 export async function POST(request: NextRequest) {
   const t = await getTranslations("login.api");
 
   const body = await request.json();
 
-  console.log(body);
+  const parsedBody = bodySchema.safeParse(body);
+
+  if (!parsedBody.success) {
+    return Response.json({
+      success: false,
+      errorCode: ERROR_CODES.INVALID_CREDENTIALS,
+      message: t("message.invalidCredentials"),
+    });
+  }
+
+  const result = await login(parsedBody.data);
+
+  if (result === ERROR_CODES.SUCCESS)
+    return Response.json({
+      success: true,
+      errorCode: ERROR_CODES.SUCCESS,
+      message: t("message.success"),
+    });
+
+  if (result === ERROR_CODES.INVALID_CREDENTIALS)
+    return Response.json({
+      success: false,
+      errorCode: ERROR_CODES.INVALID_CREDENTIALS,
+      message: t("message.invalidCredentials"),
+    });
 
   return Response.json({
-    success: true,
-    errorCode: ERROR_CODES.SUCCESS,
-    message: t("message.success"),
+    success: false,
+    errorCode: ERROR_CODES.UNKNOWN,
+    message: t("message.unknown"),
   });
 }
