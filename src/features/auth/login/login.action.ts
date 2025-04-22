fa42fa2de31e4d5f86ef5b1ba015b3a544549f29
Quibylix <barraza.fredimanuel@gmail.com>
2025-04-22 08:17:47 -0600

    feat: create login action

---
+++ src/features/auth/login/login.action.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { createClient } from "@/features/db/supabase/create-server-client.util";
+import { ERROR_CODES } from "./error_codes.constant";
+
+const SUPABASE_INVALID_CREDENTIALS = "invalid_credentials";
+
+export async function login({
+  username,
+  password,
+}: {
+  username: string;
+  password: string;
+}) {
+  const db = await createClient();
+
+  const response = await db.auth
+    .signInWithPassword({
+      email: username,
+      password,
+    })
+    .catch((err) => {
+      console.log("Error logging in:", err);
+
+      return null;
+    });
+
+  if (!response) {
+    return ERROR_CODES.UNKNOWN;
+  }
+
+  if (!response.error) {
+    return ERROR_CODES.SUCCESS;
+  }
+
+  if (response.error.code === SUPABASE_INVALID_CREDENTIALS) {
+    return ERROR_CODES.INVALID_CREDENTIALS;
+  }
+
+  return ERROR_CODES.UNKNOWN;
+}
