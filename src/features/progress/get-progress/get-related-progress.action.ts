91baeee4e7d7993ce8e498d3ddcabbeb59f34e45
Quibylix <barraza.fredimanuel@gmail.com>
2025-05-02 10:48:02 -0600

    fix: fix progress problem when the employee is not in the project

--- src/features/progress/get-progress/get-related-progress.action.ts
+++ src/features/progress/get-progress/get-related-progress.action.ts
@@ -41,17 +41,11 @@ export async function getRelatedProgress(): Promise<{
   }
 
   if (role === "employee") {
-    const { data, error } = await db
-      .from("progress")
-      .select(
-        `id, title, description, image_url, sent_date, parent_id,
-        employee (id, profile (full_name)),
-        project (id, title)`,
-      )
-      .eq("employee_id", user!.id)
-      .is("parent_id", null);
+    const { data, error } = await db.rpc("get_employee_progress", {
+      e_id: user!.id,
+    });
 
-    if (error) {
+    if (error || !data) {
       console.error("Error fetching projects:", error);
       return {
         errorCode: ERROR_CODES.UNKNOWN,
@@ -60,7 +54,6 @@ export async function getRelatedProgress(): Promise<{
     }
 
     const progress = data.map((progress) => {
-      const { project, employee } = progress;
       return {
         id: progress.id,
         title: progress.title,
@@ -69,12 +62,12 @@ export async function getRelatedProgress(): Promise<{
         sent_date: progress.sent_date,
         parent_id: progress.parent_id,
         project: {
-          id: project.id,
-          title: project.title,
+          id: progress.project_id,
+          title: progress.project_title,
         },
         employee: {
-          id: employee.id,
-          full_name: employee.profile.full_name,
+          id: progress.employee_id,
+          full_name: progress.employee_full_name,
         },
       };
     });
