d6925eaf3bf6e12585cc8e58b01cd9158a72b095
Quibylix <barraza.fredimanuel@gmail.com>
2025-05-02 12:10:15 -0600

    feat: improve progress previe styles

--- src/features/progress/get-progress/progress-preview.component.tsx
+++ src/features/progress/get-progress/progress-preview.component.tsx
@@ -1,23 +1,28 @@
+"use client";
+
 import { Card, Group, Image, Text } from "@mantine/core";
 import { ProgressData } from "./get-related-progress.action";
 import classes from "./progress-preview.module.css";
 import Link from "next/link";
 import { LocalizedDate } from "./localized-date.component";
 import { useTranslations } from "next-intl";
+import { useResizeObserver } from "@mantine/hooks";
 
 export function ProgressPreview({ progress }: { progress: ProgressData }) {
   const t = useTranslations("progress");
 
+  const [ref, rect] = useResizeObserver();
+
   return (
-    <Card withBorder radius="md" p={0} className={classes.card}>
+    <Card ref={ref} withBorder radius="md" p={0} className={classes.card}>
       <Link href={`/progress/${progress.id}`} className={classes.link}>
-        <Group wrap="nowrap" gap={0}>
+        <Group>
           <Image
+            className={
+              rect.width < 500 ? classes.imageSmall : classes.imageLarge
+            }
             src={progress.image_url ?? "/image-placeholder.jpg"}
             alt={t("alt")}
-            miw={160}
-            maw={160}
-            h={160}
           />
           <div className={classes.body}>
             <Text tt="uppercase" c="dimmed" fw={700} size="xs" lineClamp={1}>
