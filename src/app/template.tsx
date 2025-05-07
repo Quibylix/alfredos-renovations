c43dd4166da30451a3d4d7bf2e9bfb9775594439
Quibylix <barraza.fredimanuel@gmail.com>
2025-05-07 16:02:51 -0600

    refactor: add useEffect dependencies in root template

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -8,16 +8,17 @@ export default function RootTemplate({
 }: {
   children: React.ReactNode;
 }) {
+  console.clear();
   const searchParams = useSearchParams();
   const router = useRouter();
 
-  const refresh = searchParams.get("refresh");
-
   useEffect(() => {
+    const refresh = searchParams.get("refresh");
+
     if (refresh) {
       router.refresh();
     }
-  }, []);
+  }, [router, searchParams]);
 
   return <>{children}</>;
 }
