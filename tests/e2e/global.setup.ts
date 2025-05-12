d46cea6eb02fa24ebc6abafa7a452e18694508ce
Quibylix <barraza.fredimanuel@gmail.com>
2025-05-12 11:32:20 -0600

    test: move users constants to separate file

--- tests/e2e/global.setup.ts
+++ tests/e2e/global.setup.ts
@@ -1,29 +1,9 @@
 import { Database } from "@/features/db/supabase/types";
 import { expect, Page, test as setup } from "@playwright/test";
 import { createClient } from "@supabase/supabase-js";
+import { users } from "./users.constant";
 import es from "@/features/i18n/messages/es.json";
 import path from "path";
-
-export const users = {
-  boss: {
-    username: "boss",
-    password: "Password1234",
-    fullName: "Boss",
-    role: "boss",
-  },
-  employee1: {
-    username: "employee1",
-    password: "Password1234",
-    fullName: "Employee 1",
-    role: "employee",
-  },
-  employee2: {
-    username: "employee2",
-    password: "Password1234",
-    fullName: "Employee 2",
-    role: "employee",
-  },
-};
 
 async function resetDatabase() {
   const client = createClient<Database>(
---
+++ tests/e2e/users.constant.ts
@@ -0,0 +1,20 @@
+export const users = {
+  boss: {
+    username: "boss",
+    password: "Password1234",
+    fullName: "Boss",
+    role: "boss",
+  },
+  employee1: {
+    username: "employee1",
+    password: "Password1234",
+    fullName: "Employee 1",
+    role: "employee",
+  },
+  employee2: {
+    username: "employee2",
+    password: "Password1234",
+    fullName: "Employee 2",
+    role: "employee",
+  },
+};
