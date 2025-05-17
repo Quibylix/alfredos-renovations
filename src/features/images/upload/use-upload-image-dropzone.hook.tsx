bafae9745a31b96c4d2d9004fe6356bc40a3633a
Quibylix <barraza.fredimanuel@gmail.com>
2025-05-17 16:43:01 -0600

    fix: temporary solution of vercel request payload limit problem

--- src/features/images/upload/use-upload-image-dropzone.hook.tsx
+++ src/features/images/upload/use-upload-image-dropzone.hook.tsx
@@ -1,9 +1,12 @@
 import { useRef, useState } from "react";
 import { useTranslations } from "next-intl";
 import { notifications } from "@mantine/notifications";
-import { APIResponse as UploadImageAPIResponse } from "@/app/api/v1/images/upload/route";
-import { ERROR_CODES as UPLOAD_IMAGE_API_ERROR_CODES } from "./error_codes.constant";
-import { AppRoutes } from "@/features/shared/app-routes.util";
+// import { APIResponse as UploadImageAPIResponse } from "@/app/api/v1/images/upload/route";
+// import { ERROR_CODES as UPLOAD_IMAGE_API_ERROR_CODES } from "./error_codes.constant";
+// import { AppRoutes } from "@/features/shared/app-routes.util";
+import { createBrowserClient } from "@/features/db/supabase/create-browser-client.util";
+import convert from "heic-convert/browser";
+import { randomId } from "@mantine/hooks";
 
 export function useUploadImageDropzone(setImageURL: (url: string) => void) {
   const t = useTranslations("uploadImage");
@@ -23,53 +26,105 @@ export function useUploadImageDropzone(setImageURL: (url: string) => void) {
 
     const file = files[0];
 
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        image: Array.from(new Uint8Array(await file.arrayBuffer())),
-        mimeType: file.type,
-      }),
-    };
+    const db = await createBrowserClient();
 
-    fetch(AppRoutes.getRoute("API_UPLOAD_IMAGE"), options)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
+    const originalExtension = getExtensionFromMimeType(file.type);
 
-        return response.json();
-      })
-      .then((res: UploadImageAPIResponse) => handleApiResponse(res))
-      .catch((error) => {
-        console.error("Error:", error);
-        handleUnknownError();
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-
-    function handleApiResponse(res: UploadImageAPIResponse) {
-      if (res.success) {
-        setImageURL(res.data.imageURL);
-        return handleSuccessResponse();
-      }
-
-      if (res.errorCode === UPLOAD_IMAGE_API_ERROR_CODES.INVALID_REQUEST) {
-        return handleInvalidRequest();
-      }
-
-      if (
-        res.errorCode === UPLOAD_IMAGE_API_ERROR_CODES.MIME_TYPE_NOT_SUPPORTED
-      ) {
-        return handleMimeTypeNotSupported();
-      }
-
-      handleUnknownError();
+    if (originalExtension === null) {
+      return handleMimeTypeNotSupported();
     }
 
+    const extension = originalExtension === "heif" ? "jpeg" : originalExtension;
+    let imageBuffer;
+    try {
+      imageBuffer =
+        originalExtension === "heif"
+          ? await convert({
+              buffer: new Uint8Array(
+                await file.arrayBuffer(),
+              ) as unknown as ArrayBuffer,
+              format: "JPEG",
+              quality: 0.3,
+            })
+          : await file.arrayBuffer();
+    } catch (error) {
+      console.error("Error converting HEIF image:", error);
+      return handleUnknownError();
+    }
+
+    const imageName = `${randomId("image-")}.${extension}`;
+
+    const uploadResult = await db.storage
+      .from("images")
+      .upload(imageName, imageBuffer, {
+        contentType: file.type,
+      })
+      .catch((error) => {
+        console.log("Error uploading image:", error);
+        return null;
+      });
+
+    if (!uploadResult || uploadResult.error) {
+      if (uploadResult)
+        console.error("Error uploading image:", uploadResult.error);
+
+      return handleUnknownError();
+    }
+
+    const {
+      data: { publicUrl: url },
+    } = db.storage.from("images").getPublicUrl(imageName);
+
+    setImageURL(url);
+    return handleSuccessResponse();
+
+    // const options = {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     image: Array.from(new Uint8Array(await file.arrayBuffer())),
+    //     mimeType: file.type,
+    //   }),
+    // };
+    //
+    // fetch(AppRoutes.getRoute("API_UPLOAD_IMAGE"), options)
+    //   .then((response) => {
+    //     if (!response.ok) {
+    //       throw new Error("Network response was not ok");
+    //     }
+    //
+    //     return response.json();
+    //   })
+    //   .then((res: UploadImageAPIResponse) => handleApiResponse(res))
+    //   .catch((error) => {
+    //     console.error("Error:", error);
+    //     handleUnknownError();
+    //   })
+    //   .finally(() => {
+    //     setLoading(false);
+    //   });
+
+    // function handleApiResponse(res: UploadImageAPIResponse) {
+    //   if (res.success) {
+    //     setImageURL(res.data.imageURL);
+    //     return handleSuccessResponse();
+    //   }
+    //
+    //   if (res.errorCode === UPLOAD_IMAGE_API_ERROR_CODES.INVALID_REQUEST) {
+    //     return handleInvalidRequest();
+    //   }
+    //
+    //   if (
+    //     res.errorCode === UPLOAD_IMAGE_API_ERROR_CODES.MIME_TYPE_NOT_SUPPORTED
+    //   ) {
+    //     return handleMimeTypeNotSupported();
+    //   }
+    //
+    //   handleUnknownError();
+    // }
+    //
     function handleSuccessResponse() {
       notifications.show({
         title: t("success"),
@@ -77,14 +132,14 @@ export function useUploadImageDropzone(setImageURL: (url: string) => void) {
       });
     }
 
-    function handleInvalidRequest() {
-      setError(t("api.message.invalidRequest"));
-      notifications.show({
-        title: t("error"),
-        message: t("api.message.invalidRequest"),
-        color: "red",
-      });
-    }
+    // function handleInvalidRequest() {
+    //   setError(t("api.message.invalidRequest"));
+    //   notifications.show({
+    //     title: t("error"),
+    //     message: t("api.message.invalidRequest"),
+    //     color: "red",
+    //   });
+    // }
 
     function handleMimeTypeNotSupported() {
       setError(t("api.message.mimeTypeNotSupported"));
@@ -112,4 +167,21 @@ export function useUploadImageDropzone(setImageURL: (url: string) => void) {
     captureFromCamera,
     dropzoneOpenRef,
   };
+}
+
+function getExtensionFromMimeType(mimeType: string) {
+  switch (mimeType) {
+    case "image/jpeg":
+      return "jpg";
+    case "image/png":
+      return "png";
+    case "image/gif":
+      return "gif";
+    case "image/webp":
+      return "webp";
+    case "image/heif":
+      return "heif";
+    default:
+      return null;
+  }
 }
