de810fb196b93aacb9b7b8f1cb24827fd9c84b11
Quibylix <barraza.fredimanuel@gmail.com>
2025-06-25 19:43:13 -0600

    feat: update styles in media upload preview

--- src/features/media/upload/media-upload-preview.component.tsx
+++ src/features/media/upload/media-upload-preview.component.tsx
@@ -1,4 +1,10 @@
-import { CloseButton, Image, SimpleGrid, Stack } from "@mantine/core";
+import {
+  AspectRatio,
+  CloseButton,
+  Image,
+  SimpleGrid,
+  Stack,
+} from "@mantine/core";
 
 export type MediaUploadPreviewProps = {
   media: {
@@ -14,37 +20,39 @@ export function MediaUploadPreview({
 }: MediaUploadPreviewProps) {
   return (
     <SimpleGrid
-      cols={{ base: 1, sm: 4 }}
+      cols={{ base: 2, sm: 4 }}
       mt={media.length > 0 ? "xl" : 0}
       mb={media.length > 0 ? "sm" : 0}
     >
       {media.map((item, index) => (
         <Stack w="100%" h="100%" key={index} pos="relative">
-          {item.type === "image" ? (
-            <Image
-              src={item.url}
-              alt={`Media ${index + 1}`}
-              radius="md"
-              miw="100%"
-              mih="100%"
-              maw="100%"
-              mah="100%"
-              fit="cover"
-              display="block"
-              mx="auto"
-            />
-          ) : (
-            <video
-              src={item.url}
-              controls
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "cover",
-                borderRadius: "0.5rem",
-              }}
-            />
-          )}
+          <AspectRatio ratio={1}>
+            {item.type === "image" ? (
+              <Image
+                src={item.url}
+                alt={`Media ${index + 1}`}
+                radius="md"
+                miw="100%"
+                mih="100%"
+                maw="100%"
+                mah="100%"
+                fit="cover"
+                display="block"
+                mx="auto"
+              />
+            ) : (
+              <video
+                src={item.url}
+                controls
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  borderRadius: "0.5rem",
+                }}
+              />
+            )}
+          </AspectRatio>
           <CloseButton
             pos="absolute"
             top={5}
