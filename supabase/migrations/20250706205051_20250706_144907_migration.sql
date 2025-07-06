bc813d3d4cb56c939fb4d2924036460e03068166
Quibylix <barraza.fredimanuel@gmail.com>
2025-07-06 14:57:08 -0600

    chore: remove supabase policies

---
+++ supabase/migrations/20250706205051_20250706_144907_migration.sql
@@ -0,0 +1,27 @@
+drop policy "Enable bosses to see their own data" on "public"."boss";
+
+drop policy "Enable bosses to see their employees data" on "public"."employee";
+
+drop policy "Enable employees to see their own data" on "public"."employee";
+
+drop policy "Enable bosses to see their employees profiles" on "public"."profile";
+
+drop policy "Enable users to see their own profile" on "public"."profile";
+
+drop policy "Allow bosses to update their own projects" on "public"."project";
+
+drop policy "Enable bosses to create new projects" on "public"."project";
+
+drop policy "Enable bosses to see their own projects" on "public"."project";
+
+drop policy "Enable employees to see the projects they are part of" on "public"."project";
+
+drop policy "Enable bosses to create project-employee relationships" on "public"."project_employee";
+
+drop policy "Enable bosses to delete project-employee relationships" on "public"."project_employee";
+
+drop policy "Enable bosses to see the project-employee relationships they ar" on "public"."project_employee";
+
+drop policy "Enable employees to see the project-employee relationships they" on "public"."project_employee";
+
+
