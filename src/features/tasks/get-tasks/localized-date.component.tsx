779afea3b017082bffb856bf8d7f408fa2a67a61
Quibylix <barraza.fredimanuel@gmail.com>
2025-07-07 17:18:20 -0600

    feat: use real locale for dates

--- src/features/tasks/get-tasks/localized-date.component.tsx
+++ src/features/tasks/get-tasks/localized-date.component.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { Skeleton } from "@mantine/core";
+import { useLocale } from "next-intl";
 import { useEffect, useState } from "react";
 
 export function LocalizedDate({ date }: { date: Date }) {
   const [dateString, setDateString] = useState("");
+  const locale = useLocale();
 
   useEffect(() => {
     const options: Intl.DateTimeFormatOptions = {
@@ -15,7 +17,7 @@ export function LocalizedDate({ date }: { date: Date }) {
       minute: "2-digit",
     };
 
-    const formatter = new Intl.DateTimeFormat("en-US", options);
+    const formatter = new Intl.DateTimeFormat(locale, options);
     setDateString(formatter.format(date));
   }, [date]);
 
