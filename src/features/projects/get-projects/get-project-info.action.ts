d788469791b90544e8bcf95c2967cee1471bb11d
Quibylix <barraza.fredimanuel@gmail.com>
2025-07-08 11:45:47 -0600

    refactor: use prisma to get project info

--- src/features/projects/get-projects/get-project-info.action.ts
+++ src/features/projects/get-projects/get-project-info.action.ts
@@ -2,9 +2,9 @@
 
 import { ERROR_CODES } from "./error_codes.constant";
 import { User } from "@/features/db/user/user.model";
-import { createAdminClient } from "@/features/db/supabase/create-admin-client.util";
 import { USER_ROLES } from "@/features/db/user/user.constant";
 import { TaskData } from "@/features/tasks/get-tasks/get-related-tasks.action";
+import { prisma } from "@/features/db/prisma/db";
 
 export type ProjectData = {
   id: number;
@@ -16,8 +16,6 @@ export async function getProjectInfo(projectId: number): Promise<{
   errorCode: (typeof ERROR_CODES)[keyof typeof ERROR_CODES];
   project: ProjectData | null;
 }> {
-  const db = createAdminClient();
-
   const userId = await User.getCurrentUserId();
   const role = await User.getRole(userId);
 
@@ -28,86 +26,117 @@ export async function getProjectInfo(projectId: number): Promise<{
     };
   }
 
-  if (role === USER_ROLES.EMPLOYEE) {
-    const { data: validationData, error: validationError } = await db
-      .from("task_assignment")
-      .select("...task!inner(id, project(id, title))")
-      .eq("employee_id", userId!)
-      .eq("task.project_id", projectId);
+  const whereCondition =
+    role === USER_ROLES.EMPLOYEE
+      ? {
+          id: projectId,
+          task: {
+            some: { task_assignment: { some: { employee_id: userId! } } },
+          },
+        }
+      : { id: projectId };
 
-    if (!validationData || validationData.length === 0) {
-      return {
-        errorCode: ERROR_CODES.NOT_AUTHORIZED,
-        project: null,
-      };
-    }
-
-    if (validationError) {
-      console.error("Error fetching task assignment:", validationError);
-      return {
-        errorCode: ERROR_CODES.UNKNOWN,
-        project: null,
-      };
-    }
-
-    const tasksIds = validationData.map((item) => item.id);
-
-    const { data: tasksData, error: tasksError } = await db
-      .from("task")
-      .select(
-        `id, title, description, startDate: start_date, duration, completed, createdAt: created_at,
-          employees: employee(id, ...profile(fullName: full_name)),
-          media:task_media(id, type, url),
-          boss(id, ...profile(fullName: full_name)),
-          project(id, title)`,
-      )
-      .eq("project_id", projectId)
-      .in("id", tasksIds);
-
-    if (tasksError) {
-      console.error("Error fetching tasks:", tasksError);
-      return {
-        errorCode: ERROR_CODES.UNKNOWN,
-        project: null,
-      };
-    }
-
-    return {
-      errorCode: ERROR_CODES.SUCCESS,
-      project: {
-        id: projectId,
-        title: validationData[0].project.title,
-        tasks: tasksData as TaskData[],
+  const selectedColumns = {
+    id: true,
+    title: true,
+    task: {
+      ...(role === USER_ROLES.EMPLOYEE
+        ? {
+            where: {
+              task_assignment: {
+                some: { employee_id: userId! },
+              },
+            },
+          }
+        : {}),
+      orderBy: {
+        created_at: "asc" as const,
       },
-    };
-  }
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        start_date: true,
+        duration: true,
+        completed: true,
+        created_at: true,
+        task_assignment: {
+          select: {
+            employee: {
+              select: {
+                id: true,
+                profile: {
+                  select: { full_name: true },
+                },
+              },
+            },
+          },
+        },
+        task_media: {
+          select: { id: true, type: true, url: true },
+        },
+        boss: {
+          select: { id: true, profile: { select: { full_name: true } } },
+        },
+      },
+    },
+  };
 
-  const { data, error } = await db
-    .from("project")
-    .select(
-      `id, title,
-          tasks: task(
-            id, title, description, startDate: start_date, duration,
-            completed, createdAt: created_at,
-            employees: employee(id, ...profile(fullName: full_name)),
-            media:task_media (id, type, url),
-            boss(id, ...profile(fullName: full_name)),
-            project(id, title)
-          )`,
-    )
-    .eq("id", projectId)
-    .single();
+  const projectData = await prisma.project
+    .findUnique({
+      where: whereCondition,
+      select: selectedColumns,
+    })
+    .catch((error) => {
+      console.error("Error fetching project:", error);
+      return undefined;
+    });
 
-  if (error) {
-    console.error("Error fetching project:", error);
+  if (projectData === undefined) {
     return {
       errorCode: ERROR_CODES.UNKNOWN,
       project: null,
     };
   }
 
+  if (!projectData) {
+    return {
+      errorCode: ERROR_CODES.NOT_AUTHORIZED,
+      project: null,
+    };
+  }
+
   return {
     errorCode: ERROR_CODES.SUCCESS,
-    project: data as ProjectData,
+    project: {
+      id: Number(projectData.id),
+      title: projectData.title,
+      tasks: projectData.task.map((task) => ({
+        id: Number(task.id),
+        title: task.title,
+        description: task.description,
+        startDate: task.start_date.toISOString(),
+        duration: Number(task.duration),
+        completed: task.completed,
+        createdAt: task.created_at.toISOString(),
+        employees: task.task_assignment.map((assignment) => ({
+          id: assignment.employee.id,
+          fullName: assignment.employee.profile.full_name,
+        })),
+        media: task.task_media.map((media) => ({
+          id: Number(media.id),
+          type: media.type as "image" | "video",
+          url: media.url,
+        })),
+        boss: {
+          id: task.boss.id,
+          fullName: task.boss.profile.full_name,
+        },
+        project: {
+          id: Number(projectData.id),
+          title: projectData.title,
+        },
+      })),
+    },
   };
 }
