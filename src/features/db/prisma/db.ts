95e17ab522fb1d62b351fe09970848198ea5f59f
Quibylix <barraza.fredimanuel@gmail.com>
2025-07-08 11:45:31 -0600

    feat: add prisma logs in development mode

--- src/features/db/prisma/db.ts
+++ src/features/db/prisma/db.ts
@@ -2,6 +2,13 @@ import { PrismaClient } from "@/generated/prisma";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const generatePrismaClient = () =>
+  process.env.NODE_ENV === "production"
+    ? new PrismaClient()
+    : new PrismaClient({
+        log: ["query", "info", "warn", "error"],
+      });
+
+export const prisma = globalForPrisma.prisma || generatePrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
